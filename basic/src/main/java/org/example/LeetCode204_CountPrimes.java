4c20fe4116724441de81666ab6e85ad8d7aa522b
Stromwyrm-x <2443174604@qq.com>
2023-08-31 16:10:11 +0800

    完成厄拉多塞筛法，构造质数表

--- basic/src/main/java/org/example/LeetCode204_CountPrimes.java
+++ basic/src/main/java/org/example/LeetCode204_CountPrimes.java
@@ -1,19 +1,35 @@
 package org.example;
 
+import java.util.*;
+
 public class LeetCode204_CountPrimes
 {
-    //计算质数的个数
-    //todo 埃氏筛
-    public int countPrimes(int n) {
-        int count = 0;
-        for (int i=2;i<n;i++)
+    //计算质数的个数，可以构造质数表
+    //埃氏筛,即厄拉多塞筛法:如果 x 是质数，那么大于 x 的 x 的倍数 2x,3x,…一定不是质数，这里直接从x*x开始标记
+    public static int countPrimes(int n) {
+        //0表示合数，1表示质数
+        List<Integer>primeList=new ArrayList<>(Collections.nCopies(n,1));
+        int count=0;
+        for (int i = 2; i < n; i++)
         {
-            if (isPrime(i))
+            if (primeList.get(i)==1)//i是个质数
             {
                 count++;
+                if ((long)i*i<n)
+                {
+                    for (int j = i*i; j < n; j+=i)
+                    {
+                        primeList.set(j,0);
+                    }
+                }
             }
         }
         return count;
+    }
+
+    public static void main(String[] args)
+    {
+        countPrimes(10);
     }
     public boolean isPrime(int num)
     {
